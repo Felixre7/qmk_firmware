6fbd9a2690d9197d34f4eec48194fdfdc601902a
Felixre7 <felixer@protonmail.com>
2018-07-15 07:11:13 -0700

    initial commit of felix keyboards

--- keyboards/felix_fLess/config.h
+++ keyboards/felix_fLess/config.h
@@ -9,14 +9,14 @@
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Murph Inc.
 #define PRODUCT         Felix1
-#define DESCRIPTION     main + numpad.
+#define DESCRIPTION     main + numpad - fKeys.
 
 /* key matrix size */
-#define MATRIX_ROWS 6
+#define MATRIX_ROWS 5
 #define MATRIX_COLS 19
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { F7, F6, F5, F4, F3, F2 }
+#define MATRIX_ROW_PINS { F7, F6, F5, F4, F3 }
 #define MATRIX_COL_PINS { C7, C6, C5, C4, C3, C2, C1, C0, E1, E0, D7, F0, D5, D4, D3, D2 ,D1, D0, B7 }
 #define UNUSED_PINS {}
 
@@ -29,7 +29,7 @@
 #define BACKLIGHT_LEVELS 5
 
 /* Set 0 if debouncing isn't needed */
-#define DEBOUNCING_DELAY 10
+#define DEBOUNCING_DELAY 5
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 //#define LOCKING_SUPPORT_ENABLE
