7c76c110245a2b3d7d43f44be8198e34e156389c
Felixre7 <felixer@protonmail.com>
2018-07-21 21:37:59 -0700

    updated layer switching with newer QMK standard controls and got the audio working with the LCTRL key

--- keyboards/felix_fLess/rules.mk
+++ keyboards/felix_fLess/rules.mk
@@ -46,7 +46,7 @@ OPT_DEFS += -DBOOTLOADER_SIZE=4096
 #
 BOOTMAGIC_ENABLE = no	# Virtual DIP switch configuration(+1000)
 MOUSEKEY_ENABLE = no	# Mouse keys(+4700)
-EXTRAKEY_ENABLE = no	# Audio control and System control(+450)
+EXTRAKEY_ENABLE = yes	# Audio control and System control(+450)
 CONSOLE_ENABLE = no	# Console for debug(+400)
 COMMAND_ENABLE = no    # Commands for debug and configuration
 SLEEP_LED_ENABLE = no  # Breathing sleep LED during USB suspend
@@ -54,4 +54,8 @@ NKRO_ENABLE = yes		# USB Nkey Rollover - if this doesn't work, see here: https:/
 BACKLIGHT_ENABLE = no  # Enable keyboard backlight functionality
 AUDIO_ENABLE = no
 RGBLIGHT_ENABLE = no
-TAP_DANCE_ENABLE = yes
+MIDI_ENABLE = no
+UNICODE_ENABLE = no
+BLUETOOTH_ENABLE = no # Enable bluetooth with Adafruit EZ-Key HID
+SPLIT_KEYBOARD = no
+TAP_DANCE_ENABLE = no # Enables the tap dance functionality
